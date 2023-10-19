441f20ec2d191a3c224754fae3dba11c0e32434f
Anthony Elison <tonyelison37@gmail.com>
2023-10-18 19:13:19 -0600

    Add components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import { useState } from 'react'
 import './App.css'
+import EditForm from './components/EditForm.jsx'
+import ViewForm from './components/ViewForm.jsx'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="app">
+      <EditForm></EditForm>
+      <ViewForm></ViewForm>
+    </div>
   )
 }
 
